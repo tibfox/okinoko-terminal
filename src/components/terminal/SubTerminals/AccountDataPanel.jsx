551c8e8ab8bbe2124eff542486eabb5d64ef128c
tibfox <tibfox@gmail.com>
2026-04-05 21:34:04 +0000

    show nft and token contracts - remove template selection from general deployment popup

--- src/components/terminal/SubTerminals/AccountDataPanel.jsx
+++ src/components/terminal/SubTerminals/AccountDataPanel.jsx
@@ -299,6 +299,7 @@ export default function AccountDataPanel() {
           onClose={closePopup}
           aioha={capturedAioha}
           user={capturedUser}
+          hideSourceTabs
           onProcessingChange={(processing) => { deployState.isProcessing = processing }}
         />
       ),
